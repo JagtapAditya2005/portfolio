89d9f22f2d94b6c3ec3c2e3ef9445ad065087ea9
Aditya2005-ui <adityaashokjagtap9699@gmail.com>
2026-08-11 17:55:46 +0530

    Fixed image problem

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+]
+
+# Serve media files
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
 ]
 
 if settings.DEBUG:
